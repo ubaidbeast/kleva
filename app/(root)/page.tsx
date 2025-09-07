a7bb5e39968830deb7dd1fae2cb8f925c9a0d7ce
ubaidbeast <ubaidadineymar11@gmail.com>
2025-09-07 10:10:53 +0800

    added all styles and Initial file structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+
+import HeaderBox from '@/components/HeaderBox'
+import React from 'react'
+
+const Home = () => {
+  const loggedIn ={firstName: "ubaidurrahman"}
+  return (
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="access and manage your account and transactions efficiently"
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderBox = ({ type = "title", title, user, subtext }: HeaderBoxProps) => {
+  return (
+    <div className='header-box'>
+        <h1 className='header-box-title'>
+            {title}
+            {type === "greeting" &&
+              (<span className='text-bankGradient'>
+                &nbsp;{user}
+            </span>)}
+        </h1>
+    </div>
+  )
+}
+
+export default HeaderBox
