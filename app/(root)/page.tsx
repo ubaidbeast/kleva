51e349e9fb5c856d0a71f425f3071ca937c5c240
ubaidbeast <ubaidadineymar11@gmail.com>
2025-09-09 14:29:09 +0800

    added the right sidebar and bank card components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBlanceBox from '@/components/TotalBlanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn ={firstName: "ubaidurrahman"}
+  const loggedIn ={firstName: "ubaidurrahman", lastName: 'nuh', email: 'ubaidurrahman@example.com'}
   return (
     <section className='home'>
       <div className="home-content">
@@ -17,11 +18,18 @@ const Home = () => {
           />
           <TotalBlanceBox 
           accounts={[]}
-          totalBanks={[]}
+          totalBanks={1}
           totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSCTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 456.78}]}
+      />
     </section>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
+  title: "Kleva",
   description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
