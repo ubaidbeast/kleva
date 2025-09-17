987c5e55df5004665699c27c43ff3382acb76177
ubaidbeast <ubaidadineymar11@gmail.com>
2025-09-17 15:31:16 +0800

    implement auth appwrite and sentry and added footer to the navs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,20 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  const loggedeIn = {firstName: 'ubaidurrahman', lastName: 'nuh'};
+  const loggedeIn = await getLoggedInUser();
+
+  if (!loggedeIn) {
+    redirect('/sign-in');
+  }
 
   return (
     <main className="flex h-screen w-full font-inter">
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,6 +20,7 @@ import { authFormSchema } from '@/lib/utils'
 import CustomInputs from './CustomInputs'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({ type }: {type: string}) => {
     const router = useRouter();
@@ -45,21 +46,22 @@ const AuthForm = ({ type }: {type: string}) => {
 
     try {
 
-        if (type === 'sign-in') {
-            // const newaUser = await SignUp(data);
-            // setUser(newaUser);
+        if (type === 'sign-up') {
+            const newUser = await signUp(data);
+            setUser(newUser);
         }
 
-        if (type === 'sign-up') {
-            // const response = await SignIn({
-            //     email: data.email,
-            //     password: data.password,
-            // })
+        if (type === 'sign-in') {
+            const response = await signIn({
+                email: data.email,
+                password: data.password,
+            })
 
-            // if (response) router.push('/');
+            if (response) router.push('/');
         }
     } catch (error) {
-
+        console.log(error);
+        
     } finally {
         setIsLoading(false);
     }
